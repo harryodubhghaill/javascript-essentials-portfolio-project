d0857f4186ce2af30fbf9fd42c9c282641830df9
harryodubhghaill <harryodubhghaill@hotmail.com>
2021-12-21 05:41:11 +0000

    Fix API ingest and condense data for use

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,44 @@
-/**
- * Selecting the HTML elements to update through Javascript
- */
+async function loadQuestions(){
+    let rawQuestions = await (await fetch('https://opentdb.com/api.php?amount=10&type=multiple')).json();
 
-const showQuestion = document.getElementById('question');
-const answerA = document.getElementById('option-a');
-const answerB = document.getElementById('option-b');
-const answerC = document.getElementById('option-c');
-const answerD = document.getElementById('option-d');
 
- document.addEventListener("DOMContentLoaded", function() {
-    getQuestion();
-	})
-/**
- * Get question and answer data from trivia API
- */
-async function getQuestion() {
-    try {
-        const response = await fetch('https://opentdb.com/api.php?amount=1&type=multiple');
-        if (!response.ok) throw Error();
-        const data = await response.json();
-        let question = data.results[0].question;
-        let correctAnswer = data.results[0].correct_answer;
-        let incorrectAnswers = data.results[0].incorrect_answers;
-        let answerArray = incorrectAnswers.concat(correctAnswer);    //creates one array with all answers
-        console.log(question);
-        console.log(answerArray);
-        showQuestion.innerHTML = question
-        answerA.innerHTML = answerArray[0];
-        answerB.innerHTML = answerArray[1];
-        answerC.innerHTML = answerArray[2];
-        answerD.innerHTML = answerArray[3];
-        } catch (err) {
-            console.error(err);
+      let questionsArray = [];
+
+        for (let i in rawQuestions.results) {
+            let question = rawQuestions.results[i].question;
+            let correctAnswer = rawQuestions.results[i].correct_answer;
+            let incorrectAnswers = rawQuestions.results[i].incorrect_answers;
+            let answers = incorrectAnswers.concat(correctAnswer);
+
+            let questionObject = {
+                question, correctAnswer, incorrectAnswers, answers,
+            }
+
+            questionsArray.push(questionObject);
+
+            
         }
-    
-}
 
-function checkAnswer(){
+    console.log(questionsArray);
+
+    return questionsArray
+    };
+    
+
+
+document.addEventListener('DOMContentLoaded', async() => {
+    let rawQuestions = [];
+
+    try {
+        rawQuestions = await loadQuestions();
+    } catch (e) {
+        console.log('Error!');
+        console.log(e);
+    }
+})
+
+function getQuestionData() {
+    
+    let questionArray = loadQuestions();
     
 }
