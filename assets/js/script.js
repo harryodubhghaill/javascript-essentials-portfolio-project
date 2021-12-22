47d42ce07cdd7b5872a6fde4367d58f1b4bf9036
harryodubhghaill <harryodubhghaill@hotmail.com>
2021-12-22 01:06:55 +0000

    Add code for start screen difficulty choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,43 @@
 /**
+ * calls the async function when user selects difficulty
+ */
+async function easySelection() {
+    let rawQuestions = [];
+
+    try {
+        rawQuestions = await loadEasyQuestions();
+    } catch (e) {
+        console.log('Error!');
+        console.log(e);
+    }
+}
+async function mediumSelection() {
+    let rawQuestions = [];
+
+    try {
+        rawQuestions = await loadMediumQuestions();
+    } catch (e) {
+        console.log('Error!');
+        console.log(e);
+    }
+}
+async function hardSelection() {
+    let rawQuestions = [];
+
+    try {
+        rawQuestions = await loadHardQuestions();
+    } catch (e) {
+        console.log('Error!');
+        console.log(e);
+    }
+}
+
+/**
  * async function to ingest data from trivia API and sort for use
  */
-
-async function loadQuestions(){
-    let rawQuestions = await (await fetch('https://opentdb.com/api.php?amount=10&type=multiple')).json();
+//Easy Questions
+async function loadEasyQuestions(){
+    let rawQuestions = await (await fetch('https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple')).json();
 
         //creating a more suitable array for the game
       let questionsArray = [];
@@ -22,63 +56,200 @@ async function loadQuestions(){
         }
     return questionsArray
     };
+
+    //Medium Questions
+    async function loadMediumQuestions(){
+        let rawQuestions = await (await fetch('https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple')).json();
+    
+            //creating a more suitable array for the game
+          let questionsArray = [];
+    
+            for (let i in rawQuestions.results) {
+    
+                let question = rawQuestions.results[i].question;
+                let correctAnswer = rawQuestions.results[i].correct_answer;
+                let incorrectAnswers = rawQuestions.results[i].incorrect_answers;
+                let answers = incorrectAnswers.concat(correctAnswer);
+    
+                let questionObject = {
+                    question, correctAnswer, incorrectAnswers, answers,
+                }
+                questionsArray.push(questionObject); 
+            }
+        return questionsArray
+        };
+
+   //Hard Questions     
+async function loadHardQuestions(){
+    let rawQuestions = await (await fetch('https://opentdb.com/api.php?amount=10&difficulty=hard&type=multiple')).json();
+        
+        //creating a more suitable array for the game
+      let questionsArray = [];
+
+        for (let i in rawQuestions.results) {
+        
+            let question = rawQuestions.results[i].question;
+            let correctAnswer = rawQuestions.results[i].correct_answer;
+            let incorrectAnswers = rawQuestions.results[i].incorrect_answers;
+            let answers = incorrectAnswers.concat(correctAnswer);
+        
+            let questionObject = {
+               question, correctAnswer, incorrectAnswers, answers,
+                }
+                   questionsArray.push(questionObject); 
+            }   
+            return questionsArray
+            };
+
+
 /**
- * calls the async function when all DOM is loaded and catches errors
+ * Start game & difficulty selection
  */
-document.addEventListener('DOMContentLoaded', async() => {
-    let rawQuestions = [];
-
-    try {
-        rawQuestions = await loadQuestions();
-    } catch (e) {
-        console.log('Error!');
-        console.log(e);
-    }
-})
-
+// allows onclick to access internal function
 let query;
 /**
- * injects and renders data on page
+ * runs the game (different instances for difficulty)
  */
-function renderQuestion(){
+//Easy Game
+function runEasyGame(){
 
-    let data = loadQuestions();
+    document.getElementById("next").style.visibility = "hidden"
+
+    let data = loadEasyQuestions();
 
     data.then(function(result) {    //used .then to resolve a promise
 
         let singleQuestion = result[0];
-        let correctAnswer = result[0].correctAnswer
+        let correctAnswer = result[0].correctAnswer;
 
   
       document.getElementById('question').innerHTML = singleQuestion.question;
 
-      document.getElementById('option-a').innerHTML = singleQuestion.answers[0];
-      document.getElementById('option-b').innerHTML = singleQuestion.answers[1];
-      document.getElementById('option-c').innerHTML = singleQuestion.answers[2];
-      document.getElementById('option-d').innerHTML = singleQuestion.answers[3];
+      document.getElementById('option-a').innerHTML = singleQuestion.answers[1];
+      document.getElementById('option-b').innerHTML = singleQuestion.answers[3];
+      document.getElementById('option-c').innerHTML = singleQuestion.answers[0];
+      document.getElementById('option-d').innerHTML = singleQuestion.answers[2];
 
      function getUserAnswer(userAnswer){
         if (userAnswer === correctAnswer) {
-            console.log('correct')
+            alert('correct');
+            function incrementScore() {
+
+                // Gets the current score from the DOM and increments it
+            
+                let oldScore = parseInt(document.getElementById("score").innerText);
+                document.getElementById("score").innerText = ++oldScore;
+            }
+            incrementScore();
+            document.getElementById("next").style.visibility = "visible";
+
         } else {
-            console.log('Nope, you dummy!')
+            alert('Nope, you dummy!');
+            document.getElementById("next").style.visibility = "visible";
         }
-        console.log(userAnswer);
     }
     query = getUserAnswer;
     })
 }
-renderQuestion();
+//Medium Game
+function runMediumGame(){
 
-/**
- * Gets users answer
- */
+    document.getElementById("next").style.visibility = "hidden"
 
-/** 
- * Checks to see if user answer === correct answer
-*/
-function checkAnswer() {
+    let data = loadMediumQuestions();
 
-    console.log('running');
-    
+    data.then(function(result) {    //used .then to resolve a promise
+
+        let singleQuestion = result[0];
+        let correctAnswer = result[0].correctAnswer;
+
+  
+      document.getElementById('question').innerHTML = singleQuestion.question;
+
+      document.getElementById('option-a').innerHTML = singleQuestion.answers[1];
+      document.getElementById('option-b').innerHTML = singleQuestion.answers[3];
+      document.getElementById('option-c').innerHTML = singleQuestion.answers[0];
+      document.getElementById('option-d').innerHTML = singleQuestion.answers[2];
+
+     function getUserAnswer(userAnswer){
+        if (userAnswer === correctAnswer) {
+            alert('correct');
+            function incrementScore() {
+
+                // Gets the current score from the DOM and increments it
+            
+                let oldScore = parseInt(document.getElementById("score").innerText);
+                document.getElementById("score").innerText = ++oldScore;
+            }
+            incrementScore();
+            document.getElementById("next").style.visibility = "visible";
+
+        } else {
+            alert('Nope, you dummy!');
+            document.getElementById("next").style.visibility = "visible";
+        }
     }
+    query = getUserAnswer;
+    })
+}
+//Hard Game
+function runHardGame(){
+
+    document.getElementById("next").style.visibility = "hidden"
+
+    let data = loadHardQuestions();
+
+    data.then(function(result) {    //used .then to resolve a promise
+
+        let singleQuestion = result[0];
+        let correctAnswer = result[0].correctAnswer;
+
+  
+      document.getElementById('question').innerHTML = singleQuestion.question;
+
+      document.getElementById('option-a').innerHTML = singleQuestion.answers[1];
+      document.getElementById('option-b').innerHTML = singleQuestion.answers[3];
+      document.getElementById('option-c').innerHTML = singleQuestion.answers[0];
+      document.getElementById('option-d').innerHTML = singleQuestion.answers[2];
+
+     function getUserAnswer(userAnswer){
+        if (userAnswer === correctAnswer) {
+            alert('correct');
+            function incrementScore() {
+
+                // Gets the current score from the DOM and increments it
+            
+                let oldScore = parseInt(document.getElementById("score").innerText);
+                document.getElementById("score").innerText = ++oldScore;
+            }
+            incrementScore();
+            document.getElementById("next").style.visibility = "visible";
+
+        } else {
+            alert('Nope, you dummy!');
+            document.getElementById("next").style.visibility = "visible";
+        }
+    }
+    query = getUserAnswer;
+    })
+}
+/**
+ * funcions attached to on click listeners for difficulty selection
+ */
+function easyGame(){
+    document.getElementById('start-container').style.display = "none"
+    document.getElementById('quiz-container').style.display = 'flex';
+    runEasyGame();
+}
+
+function mediumGame(){
+    document.getElementById('start-container').style.display = "none"
+    document.getElementById('quiz-container').style.display = 'flex';
+    runMediumGame();
+}
+
+function hardGame(){
+    document.getElementById('start-container').style.display = "none"
+    document.getElementById('quiz-container').style.display = 'flex';
+    runHardGame();
+}
